22236168f0ed2895afaa7275ee3a8d5458c76b43
CaravanTechniker <aerophoto.de@gmail.com>
2026-02-20 02:16:49 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,39 +1,12 @@
-const CACHE = "caravan-tam-cache-v0.1.5";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./app.js",
-  "./content.json",
-  "./manifest.webmanifest"
-];
-
+// sw.js – TEMPORARY DISABLED SERVICE WORKER
 self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
+  e.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", (e) => {
-  const url = new URL(e.request.url);
-  if (url.origin !== location.origin) return;
-
-  e.respondWith(
-    caches.match(e.request).then(cached => {
-      const fetchPromise = fetch(e.request).then(resp => {
-        const copy = resp.clone();
-        caches.open(CACHE).then(cache => cache.put(e.request, copy));
-        return resp;
-      }).catch(() => cached);
-
-      return cached || fetchPromise;
-    })
-  );
+  // no cache, always network
 });
